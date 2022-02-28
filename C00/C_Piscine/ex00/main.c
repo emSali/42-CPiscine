fbc92af86693136715feb0fe820dcee55259b457
emSali <50519189+emSali@users.noreply.github.com>
2022-02-28 18:08:31 +0000

    Delete C00/C_Piscine directory

--- C00/C_Piscine/ex00/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void ft_putchar(char);
-
-int main(void)
-{
-	ft_putchar('c');
-}
--- C00/C_Piscine/ex07/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void ft_putnbr(int nb);
-
-int main(void)
-{
-	ft_putnbr(-2147483648);
-}
